d379d5dda89ad56feb128cab4adb61e8c23b7168
mznbogdan <mznbogdan@gmail.com>
2024-11-29 21:48:42 +0100

    - updated Description Activity

--- app/src/main/java/com/example/tournamentsandgames/ui/tournaments/TournamentDescriptionActivity.kt
+++ app/src/main/java/com/example/tournamentsandgames/ui/tournaments/TournamentDescriptionActivity.kt
@@ -8,9 +8,12 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,14 +24,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -46,14 +53,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tournamentsandgames.R
 import com.example.tournamentsandgames.data.model.Team
 import com.example.tournamentsandgames.data.model.Tournament
 import com.example.tournamentsandgames.data.repository.FirebaseResult
 import com.example.tournamentsandgames.ui.home.Home
+import com.example.tournamentsandgames.ui.home.ui.theme.colorMain
+import com.example.tournamentsandgames.ui.home.ui.theme.darkTint
 import com.example.tournamentsandgames.ui.home.ui.theme.primaryColor
+import com.example.tournamentsandgames.ui.tournamentProcess.SummaryTeamRaw
 import com.example.tournamentsandgames.ui.tournamentProcess.TournamentsProcessActivity
 import com.example.tournamentsandgames.ui.tournaments.ui.theme.TournamentsAndGamesTheme
 
@@ -222,6 +236,7 @@ fun DisplayTournamentDetails(
                         text = "Turniej zakończony:",
                         style = MaterialTheme.typography.headlineSmall
                     )
+
                 }
 
                 val teams = tournament.teams
@@ -232,8 +247,12 @@ fun DisplayTournamentDetails(
                             .padding(top = 8.dp)
                             .heightIn(max = 200.dp)
                     ) {
-                        items(teams) { team ->
-                            TeamCardWithScoreInput(team)
+                        itemsIndexed(teams) { idx, team ->
+                            DescriptionTeamRaw(
+                                team = team,
+                                place = idx,
+                                tournament.ended
+                            )
                         }
                     }
                     Button(
@@ -249,7 +268,7 @@ fun DisplayTournamentDetails(
                         shape = RoundedCornerShape(10.dp),
                         colors = ButtonDefaults.buttonColors(primaryColor)
                     ) {
-                        Text("Ok")
+                        Text("OK")
                     }
                 }
             }
@@ -260,22 +279,56 @@ fun DisplayTournamentDetails(
 }
 
 @Composable
-fun TeamCardWithScoreInput(
-    team: Team,
-) {
-    var score by remember { mutableStateOf(team.points) }
-
+fun DescriptionTeamRaw(team: Team, place: Int, ended: Boolean) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        elevation = CardDefaults.cardElevation(8.dp)
+            .padding(vertical = 3.dp),
+        elevation = CardDefaults.cardElevation(1.dp),
+        colors = CardDefaults.cardColors(containerColor = darkTint.copy(alpha = 0.8f))
     ) {
         Column(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(darkTint.copy(alpha = 0.8f))
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = team.name, style = MaterialTheme.typography.headlineSmall)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(darkTint.copy(alpha = 0.8f)),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "${place+1}.",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier.weight(0.1f),
+                    color = colorMain.copy(alpha = 0.7f)
+                )
+                Text(
+                    text = team.name,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier.weight(0.4f),
+                    color = Color.Black.copy(alpha = 0.7f)
+                )
+                if (ended) {
+                    Text(
+                        text = "Punkty: ${team.points}",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.weight(0.3f),
+                        textAlign = TextAlign.End,
+                        color = Color.Black.copy(alpha = 0.7f)
+                    )
+                }
+            }
         }
     }
 }
